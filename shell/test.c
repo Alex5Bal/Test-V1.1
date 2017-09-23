0f058fe57d44f565690f4c2b7249f47ebe5cfd09
Alex5Bal <abalderrama5@outlook.com>
2017-09-23 17:03:39 -0600

    Add files via upload

--- shell/test.c
+++ shell/test.c
@@ -21,16 +21,17 @@ int main()
 	{
 		char *string = (char*) malloc(BUFFERSIZE); /****String used to hold the input.****/
 
-		write(1, "$ ", 1);
+		write(1, "$ ", 2);
 		read(0, string, BUFFERSIZE); /****Reads the input to the string variable.****/
+
+		int tokenCount = tokenCounter(string, delim);
+		printf("token count: %d\n", tokenCount);
 
 		if(strComp(string, "exit\n"))
 			exit(0);
 
 		else
 		{
-			int tokenCount = tokenCounter(string, delim);
-			printf("token count: %d\n", tokenCount);
 			char** tokenVec = Mytoc(string, delim); /****Call to Mytoc() function to obtain a vector of tokens.****/
 
 			for (i = 0; i < tokenCount; i++) /****Loop to print the contents of the token vector.****/
