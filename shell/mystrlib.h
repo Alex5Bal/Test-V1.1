dd77540814e592ddcbc3e825d6e02de951e46492
Alex5Bal <abalderrama5@outlook.com>
2017-09-24 17:25:42 -0600

    Delete mystrlib.h

--- shell/mystrlib.h
+++
@@ -1,9 +0,0 @@
-#ifndef mystrlib_included
-#define mystrlib_included
-
-char strComp(char* string1, char* string2);
-int strLen(char* stringIn);
-char* strCat(char* string1, char* string2);
-char* strCopy(char* stringIn);
-
-#endif
