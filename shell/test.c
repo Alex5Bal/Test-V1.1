b3956c62928ac030be82a298cc5fd58b50fc6787
Alex5Bal <abalderrama5@outlook.com>
2017-09-24 09:13:33 -0600

    Add files via upload

--- shell/test.c
+++ shell/test.c
@@ -10,42 +10,40 @@
 #include "mytoc.h"
 #include "mystrlib.h"
 
-#define BUFFERSIZE 200 /****Defines the max size of buffer.****/
+#define BUFFERSIZE 150 /****Defines the max size of buffer.****/
 
 int main()
 {
 	int i, tokenCount;
 	char delim = ' ';
+	char* string;
+	char** tokenVec;
 
 	while(1) /****Program runs input prompt until X is typed in.****/
 	{
-		char *string = (char*) malloc(BUFFERSIZE); /****String used to hold the input.****/
+		string = (char*) malloc(BUFFERSIZE); /****String used to hold the input.****/
 
 		write(1, "$ ", 2);
 		read(0, string, BUFFERSIZE); /****Reads the input to the string variable.****/
 
-		tokenCount = tokenCounter(string, delim);
-		printf("token count: %d\n", tokenCount);
-
-		if(strComp(string, "exit\n"))
+		if(string[0] == 'X')
 			exit(0);
 
 		else
 		{
-			char** tokenVec = Mytoc(string, delim); /****Call to Mytoc() function to obtain a vector of tokens.****/
+			tokenCount = tokenCounter(string, delim);
+			tokenVec = Mytoc(string, delim); /****Call to Mytoc() function to obtain a vector of tokens.****/
 
 			for (i = 0; i < tokenCount; i++) /****Loop to print the contents of the token vector.****/
 			{
-				printf("[%d] : ", i);
-				printf("%s", tokenVec[i]);
-				printf("\n");
+				printf("[%d] : %s\n", i, tokenVec[i]);
 			}
 
-			for (i = 0; i < tokenCount; i++)
+			for (i = 0; tokenVec[i] != '\0'; i++)
 			{
 				free(tokenVec[i]);
 			}
-			//****Frees the memory allocated to the token vector before moving on the the next input.****/
+			free(tokenVec);//****Frees the memory allocated to the token vector before moving on the the next input.****/
 		}
 		free(string);
 		tokenCount = 0;
