b970dd9962db2abec6f2d76da2b8a578d23baff4
Alex5Bal <abalderrama5@outlook.com>
2017-09-24 17:56:24 -0600

    Add files via upload

---
+++ shell/mystrlib.h
@@ -0,0 +1,8 @@
+#ifndef mystrlib_included
+#define mystrlib_included
+
+int strComp(char* string1, char* string2);
+int strLen(char* stringIn);
+char* strCat(char* string1, char* string2);
+
+#endif
---
+++ shell/shell.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include "mytoc.h"
+#include "mystrlib.h"
+#include "shell.h"
+
+#define BUFFERSIZE 150
+
+int main(int argc, char** argv, char** envp)
+{
+	char** args, path;
+	char* string, program;
+	pid_t pid;
+
+	while(1)
+	{
+		string = (char*) malloc(BUFFERSIZE);
+
+		write(1, "$ ", 2);
+		read(0, string, BUFFERSIZE);
+
+		if(strCompare(string, "exit\n"))
+			exit(0);
+
+		else
+		{
+			args = Mytoc(string, ' ');
+
+			pid = fork();
+
+			if(pid < 0)
+			{
+				fprintf(stderr, "fork() failed\n");
+				exit(1);
+			}
+			else if(pid == 0)
+			{
+				printf(pid);
+			}
+		}
+	}
+}
