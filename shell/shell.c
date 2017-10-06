4e0bdd0ddf4b7902ef43bbebe910fbfa71788b22
Alex5Bal <abalderrama5@outlook.com>
2017-10-06 14:15:21 -0600

    Add files via upload

--- shell/shell.c
+++ shell/shell.c
@@ -78,7 +78,6 @@ int main(int argc, char** argv, char** envp)
 					{
 						dup2(pipe1[0], STDIN_FILENO);
 						close(pipe1[1]);
-						//dup2(pipe2[1], STDOUT_FILENO);  	//Appended (Remove if needed!)
 						close(pipe2[0]);					//Appended (Remove if needed!)
 						execve("/bin/wc", args, envp);
 						printf("Command not found\n");	//No valid commands were identified
@@ -96,47 +95,47 @@ int main(int argc, char** argv, char** envp)
 				}
 			}
 
-			else
-			{
-				if(pid < 0)		//Child process creation was unsuccessful
-				{
-					perror("Fork failed\n");
-					exit(1);
-				}
-				else if(pid == 0)	//Child process created successfully
-				{
-					for(int i = 0; envp[i] != (char*)0; i++)	//Traverse envp[]
-					{
-						tempVec = Mytoc(envp[i], '=');	//Identify key and value by tokenizing with '='
-
-						if(strComp(tempVec[0], "PATH"))	//If the key value is PATH
-						{
-							path = Mytoc(tempVec[1], ':');	//Tokenize the value of PATH with ':'
-							break;	//Exit loop, path has been found
-						}
-
-						free(tempVec);	//Ready for next iteration
-					}
-
-					command = strCat("/", args[0]);	//Ready user command to be used w/ the path
-
-					for(int j = 0; path[j] != '\0'; j++)	//Traverse the path vector until valid path is executed
-					{
-						free(args[0]);	//Ready args[0] to store the complete path
-						args[0] = strCat(path[j], command);	//args[0] contains complete path
-						printf("%s\n", args[0]);
-						execve(args[0], args, envp);	//Execute the complete path if valid
-					}
-
-					printf("Command not found\n");	//No valid commands were identified
-					exit(0);
-				}
-
-				else
-				{
-					wait(NULL);	//Wait for the child process to end
-				}
-			}
+//			else
+//			{
+//				if(pid < 0)		//Child process creation was unsuccessful
+//				{
+//					perror("Fork failed\n");
+//					exit(1);
+//				}
+//				else if(pid == 0)	//Child process created successfully
+//				{
+//					for(int i = 0; envp[i] != (char*)0; i++)	//Traverse envp[]
+//					{
+//						tempVec = Mytoc(envp[i], '=');	//Identify key and value by tokenizing with '='
+//
+//						if(strComp(tempVec[0], "PATH"))	//If the key value is PATH
+//						{
+//							path = Mytoc(tempVec[1], ':');	//Tokenize the value of PATH with ':'
+//							break;	//Exit loop, path has been found
+//						}
+//
+//						free(tempVec);	//Ready for next iteration
+//					}
+//
+//					command = strCat("/", args[0]);	//Ready user command to be used w/ the path
+//
+//					for(int j = 0; path[j] != '\0'; j++)	//Traverse the path vector until valid path is executed
+//					{
+//						free(args[0]);	//Ready args[0] to store the complete path
+//						args[0] = strCat(path[j], command);	//args[0] contains complete path
+//						printf("%s\n", args[0]);
+//						execve(args[0], args, envp);	//Execute the complete path if valid
+//					}
+//
+//					printf("Command not found\n");	//No valid commands were identified
+//					exit(0);
+//				}
+//
+//				else
+//				{
+//					wait(NULL);	//Wait for the child process to end
+//				}
+//			}
 
 			free(string);	//Ready string for next buffer input
 
